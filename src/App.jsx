530f9a4f795cc995b771687397b95d70ea73b35e
Rafly <Rutherford9908@gmail.com>
2025-09-30 16:09:52 +0700

    fix inconsistent background

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ export default function App() {
 
 
   return (
-    <div className="min-h-screen bg-neutral-50 text-neutral-900 dark:bg-neutral-950 dark:text-neutral-100">
+    <div className="min-h-screen bg-black text-white">
       <Navbar />
       <ScrollProgress />
       <CursorBlob />
